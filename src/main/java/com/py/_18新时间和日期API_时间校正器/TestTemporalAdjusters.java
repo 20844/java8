f8f6d64e5d6529ee67feb840c7c5f92495516d54
pengyang <20844@qq.com>
2023-08-31 21:23:40 +0800

    新时间和日期 API-时间校正器

---
+++ src/main/java/com/py/_18新时间和日期API_时间校正器/TestTemporalAdjusters.java
@@ -0,0 +1,39 @@
+package com.py._18新时间和日期API_时间校正器;
+
+import org.junit.Test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
+
+public class TestTemporalAdjusters {
+    @Test
+    public void test01(){
+        //TemporalAdjusters：时间校正器
+        LocalDateTime ldt1 = LocalDateTime.now();
+        System.out.println(ldt1);
+
+        //指定日期时间中的 年 月 日 ...
+        LocalDateTime ldt2 = ldt1.withDayOfMonth(10);
+        System.out.println(ldt2);
+
+        //指定时间校正器
+        LocalDateTime ldt3 = ldt1.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+        System.out.println(ldt3);
+
+        //自定义时间校正器
+        LocalDateTime ldt5 = ldt1.with((ta) -> {
+            LocalDateTime ldt4 = (LocalDateTime) ta;
+            DayOfWeek dow1 = ldt4.getDayOfWeek();
+            if (dow1.equals(DayOfWeek.FRIDAY)) {
+                return ldt4.plusDays(3);
+            } else if (dow1.equals(DayOfWeek.SATURDAY)) {
+                return ldt4.plusDays(2);
+            } else {
+                return ldt4.plusDays(1);
+            }
+        });
+        System.out.println(ldt5);
+    }
+
+}
